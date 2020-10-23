64d4cf4f65583222454e4d552646b6a36517a0dc
root <root@vultr.guest>
2020-10-23 01:28:56 +0000

    add py3.7 and pytorch script with dockerfile

---
+++ devel_cudnn7_py3.7.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+export IMAGE_NAME="nvidia/cuda"
+export CUDA_VERSION="10.2"
+export OS="ubuntu16.04"
+export ARCH=`uname -m`
+
+docker build -t "${IMAGE_NAME}:${CUDA_VERSION}-devel-cudnn7-py3.7-${OS}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" "dist/${CUDA_VERSION}/${OS}-${ARCH}/devel/cudnn7/python3.7/"
